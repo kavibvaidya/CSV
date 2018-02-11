1d4c198da022339e5e5f51afc03f1bc6651c042e
Vaidya@finsix <Vaidya@Vaidya.local>
2018-02-11 14:55:00 -0800

    So it begins

---
+++ make_csv.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+import csv, argparse
+
+def produce(symbol):
+	url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=" + symbol + "&apikey=DK1XS6X3NJ8R5F0O&datatype=csv"
+	response = requests.get(url)
+	# Print the status code of the response.
+	#print(response.text)
+
+	parser = csv.reader(response.text.split('\n'))
+	#converts the requests into csv file
+	f = open('result.csv', "w")
+	writer = csv.writer(f)
+	count =0
+	for i in parser:
+		if len(i) != 0:
+			writer.writerow((i[1],i[2],i[2],i[3],i[4],i[6],i[5]))
+
+	f.close()
+
+
+produce("AAPL")
